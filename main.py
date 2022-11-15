3a3a14c22fbbb4cc0b3578c2f5f95b3fc4adaa05
VertX00D <118073680+VertX00D@users.noreply.github.com>
2022-11-15 17:10:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 from base64 import b64decode
 from json import loads as json_loads, load
 from ctypes import windll, wintypes, byref, cdll, Structure, POINTER, c_char, c_buffer
-from urllib.request import Request, urlopen
+from urllib.request import Request, urlopen  
 from json import *
 import time  
 import shutil
