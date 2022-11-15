5f441f27b022e8811f9de9148a6af847b1d9c2a9
VertX00D <118073680+VertX00D@users.noreply.github.com>
2022-11-15 21:55:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import threading
 from sys import executable
 from sqlite3 import connect as sql_connect
 import re
-from base64 import b64decode
+from base64 import b64decode  
 from json import loads as json_loads, load
 from ctypes import windll, wintypes, byref, cdll, Structure, POINTER, c_char, c_buffer
 from urllib.request import Request, urlopen  
